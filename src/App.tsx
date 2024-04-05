d0ccf48c9ded62b9975317318358bb342cea7210
Giancarlo <giancarlodev0705@gmail.com>
2024-04-05 12:25:01 -0700

    - added prettier and settings for prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className=" text-center">
+    <div className="text-center">
       <header className="App-header">
-        <p>This is git history explorer!{' '}
-        <a
-          className="App-link"
-          href="https://github.com/gian0705/Github_commit_history"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Repository URL
-        </a>
+        <p>
+          This is git history explorer!{" "}
+          <a
+            className="App-link"
+            href="https://github.com/gian0705/Github_commit_history"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Repository URL
+          </a>
         </p>
-        
       </header>
     </div>
   );
